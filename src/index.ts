1259536749cc3bb9dee85975474c334c2872964f
Muhammad Zafar <mzafar611@gmail.com>
2023-08-04 20:34:19 +0500

    Initialize Express app with routes and MongoDB connection

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { userRouter } from "./routes/User";
+import { CourseRouter } from "./routes/Course";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use("/User", userRouter);
+app.use("/Course", CourseRouter);
+
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE);
+
+// Check if the connection was successful
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log("server is running");
+});
