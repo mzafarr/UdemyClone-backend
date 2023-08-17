969c53d9614b562545c8ab2b159c8a802434f606
Muhammad Zafar <mzafar611@gmail.com>
2023-08-17 06:49:42 +0500

    commit

--- src/index.ts
+++ src/index.ts
@@ -17,23 +17,14 @@ app.use("/Course", CourseRouter);
 
 const PORT = process.env.PORT || 3000;
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.DATABASE);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
-//Routes go here
-app.all("*", (req, res) => {
-  res.json({ "every thing": "is awesome" });
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE);
+
+// Check if the connection was successful
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
 });
 
-//Connect to the database before listening
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("listening for requests");
-  });
+app.listen(PORT, () => {
+  console.log("server is running on ", PORT);
 });
